4af044e05296f996ce13ef02f751ed6362012ab9
jssuzanne <jssuzanne@anybox.fr>
2017-03-04 00:40:42 +0100

    Update unit test

--- src/reducers/global.js
+++ src/reducers/global.js
@@ -1,4 +1,13 @@
-const defaultState = {
+/**
+This file is a part of the FuretUI project
+
+   Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file,You can
+obtain one at http://mozilla.org/MPL/2.0/.
+**/
+export const defaultState = {
     title: '',
     custom_view: '',
     modal_custom_view: '',
@@ -11,10 +20,12 @@ export const global = (state = defaultState, action) => {
     switch (action.type) {
         case 'UPDATE_GLOBAL':
             const _state = Object.assign({}, defaultState);
-            delete _state.title;
-            if (action.custom_view || action.spaceId) return Object.assign({}, _state, value);
-            if (action.modal_custom_view) return Object.assign({}, state, value);
-            return state;
+            if (state.title) {
+                delete _state.title;
+                delete _state.modal_custom_view;
+            }
+            if (action.custom_view || action.spaceId) return Object.assign({}, state, _state, value);
+            else return Object.assign({}, state, value);
         case 'CLEAR_GLOBAL':
             return defaultState;
         default:
