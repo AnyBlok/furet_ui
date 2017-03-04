c343f947e3d3064b40234a6c3507d12c1795d186
jssuzanne <jssuzanne@anybox.fr>
2017-03-04 22:31:45 +0100

    Update unit test

--- src/reducers/views.js
+++ src/reducers/views.js
@@ -1,4 +1,13 @@
-const defaultState = {}
+/**
+This file is a part of the FuretUI project
+
+   Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file,You can
+obtain one at http://mozilla.org/MPL/2.0/.
+**/
+export const defaultState = {}
 
 export const views = (state = defaultState, view) => {
     const value = Object.assign({}, view);
