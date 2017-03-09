f98412167e151120969a8d1b50777f3fc3c4b56b
jssuzanne <jssuzanne@anybox.fr>
2017-03-09 11:28:45 +0100

    Update exisiting field: URL

--- src/fields/url.js
+++ src/fields/url.js
@@ -9,68 +9,49 @@ obtain one at http://mozilla.org/MPL/2.0/.
 **/
 import React from 'react';
 import plugin from '../plugin';
-import TextField from 'material-ui/TextField';
-import ActionHome from 'material-ui/svg-icons/action/open-in-browser';
-import {indigo500} from 'material-ui/styles/colors';
+import {BaseList, BaseThumbnail, BaseForm} from './base';
 
-
-export class URLList extends React.Component {
-    render () {
+export class URLList extends BaseList {
+    getInput () {
         return (
-            <a href={this.props.value}>{this.props.value}</a>
+            <a href={this.value}>{this.value}</a>
         );
     }
 }
 
-export class URLThumbnail extends React.Component {
-    render () {
-        const fullWidth = Boolean(eval(this.props.fullwidth));
+export class URLThumbnail extends BaseThumbnail {
+    getInput () {
         return (
-            <div
-                style={{width: fullWidth ? '100%' : 256,
-                        display: 'inline-block',
-                }}
-            >
-                <TextField
-                    id={this.props.id}
-                    floatingLabelStyle={{width: '90%'}}
-                    style={{width: '90%'}}
-                    type='url'
-                    floatingLabelText={this.props.label}
-                    disabled={true}
-                    value={this.props.value}
-                />
-                <a href={this.props.value} style={{width: '10%'}}><i className='fa fa-external-link fa-lg' /></a>
+            <div>
+                <a href={this.value}>{this.value}</a>
             </div>
         );
     }
 }
-
-export class URLForm extends React.Component {
-    render () {
-        if (this.props.readonly) return <URLThumbnail {...this.props} />
-        const required= Boolean(eval(this.props.required));
-        let error = ''
-        if (required && !this.props.readonly && !this.props.value) {
-            error = 'This field is required';
-        } else {
+export class URLForm extends BaseForm {
+    updateThisData () {
+        super.updateThisData();
+        if (!this.error) {
             const reg = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
-            if (!reg.test(this.props.value)) error = 'It is not an url';
+            if (!reg.test(this.value)) this.error = 'It is not an url';
         }
-        const floatingLabelStyle = {};
-        if (required) floatingLabelStyle.color = indigo500;
+    }
+    getInputProps () {
+        const props = super.getInputProps();
+        props.type = 'text';
+        return props;
+    }
+    getInput () {
         return (
-            <TextField
-                id={this.props.id}
-                floatingLabelText={this.props.label}
-                floatingLabelStyle={floatingLabelStyle}
-                fullWidth={Boolean(eval(this.props.fullwidth))}
-                required={Boolean(eval(this.props.required))}
-                value={this.props.value}
-                onChange={(e) => this.props.onChange(this.props.name, e.target.value)}
-                errorText={error}
-            />
-        );
+            <div className="input-group">
+                {super.getInput()}
+                <span className="input-group-addon">
+                    <a href={this.value}>
+                        <i className='fa fa-external-link fa-lg' />
+                    </a>
+                </span>
+            </div>
+        )
     }
 }
 
@@ -81,5 +62,5 @@ plugin.set(['field', 'Form'], {'URL': URLForm});
 export default {
     URLList,
     URLThumbnail,
-    // StringForm,
+    URLForm,
 }
