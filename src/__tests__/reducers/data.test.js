a89b470a622b26952df85621a5c80ef07ed69f78
jssuzanne <jssuzanne@anybox.fr>
2017-03-24 11:52:33 +0100

    Add unit test for DELETE_DATA

--- src/__tests__/reducers/data.test.js
+++ src/__tests__/reducers/data.test.js
@@ -20,7 +20,6 @@ test('multi action 1', () => {
         {type: 'UPDATE_DATA', model: 'Model', data: {'1': {id: '1', title: 'Test'}}}
     )).to.deep.equal(expected);
 });
-
 test('multi action 2', () => {
     const expected = Object.assign({}, defaultState, {'Model': {'1': {id: '1', other: 'Test'}}});
     chai.expect(reducer(
@@ -28,10 +27,134 @@ test('multi action 2', () => {
         {type: 'UPDATE_DATA', model: 'Model', data: {'1': {id: '1', other: 'Test'}}}
     )).to.deep.equal(expected);
 });
-
 test('get clear all', () => {
     chai.expect(reducer(
         Object.assign({}, defaultState, {title: 'Test'}),
         {type: 'CLEAR_DATA'}
     )).to.deep.equal(defaultState);
 });
+test('delete data clear data', () => {
+    const state = {
+        'Model': {
+            '1': {
+                name: 'Name 1',
+                title: 'Title 2',
+            },
+        },
+    }
+    const action = {
+        type: 'DELETE_DATA',
+        data: {
+            'Model': ['1'],
+        },
+    }
+    const expected = {};
+    chai.expect(reducer(state, action)).to.deep.equal(expected);
+})
+test('delete data clear data in multi value', () => {
+    const state = {
+        'Model': {
+            '1': {
+                name: 'Name 1',
+                title: 'Title 2',
+            },
+            '2': {
+                name: 'Name 2',
+                title: 'Title 2',
+            },
+        },
+    }
+    const action = {
+        type: 'DELETE_DATA',
+        data: {
+            'Model': ['1'],
+        },
+    }
+    const expected = {
+        'Model': {
+            '2': {
+                name: 'Name 2',
+                title: 'Title 2',
+            },
+        },
+    };
+    chai.expect(reducer(state, action)).to.deep.equal(expected);
+})
+test('delete data clear data in multi value', () => {
+    const state = {
+        'Model': {
+            '1': {
+                name: 'Name 1',
+                title: 'Title 2',
+            },
+            '2': {
+                name: 'Name 2',
+                title: 'Title 2',
+            },
+        },
+    }
+    const action = {
+        type: 'DELETE_DATA',
+        data: {
+            'Model': ['1', '2'],
+        },
+    }
+    const expected = {};
+    chai.expect(reducer(state, action)).to.deep.equal(expected);
+})
+test('delete data clear data in multi value 2', () => {
+    const state = {
+        'Model': {
+            '1': {
+                name: 'Name 1',
+                title: 'Title 2',
+            },
+        },
+        'Model2': {
+            '2': {
+                name: 'Name 2',
+                title: 'Title 2',
+            },
+        },
+    }
+    const action = {
+        type: 'DELETE_DATA',
+        data: {
+            'Model': ['1'],
+        },
+    }
+    const expected = {
+        'Model2': {
+            '2': {
+                name: 'Name 2',
+                title: 'Title 2',
+            },
+        },
+    };
+    chai.expect(reducer(state, action)).to.deep.equal(expected);
+})
+test('delete data clear data in multi value 3', () => {
+    const state = {
+        'Model2': {
+            '2': {
+                name: 'Name 2',
+                title: 'Title 2',
+            },
+        },
+    }
+    const action = {
+        type: 'DELETE_DATA',
+        data: {
+            'Model': ['1'],
+        },
+    }
+    const expected = {
+        'Model2': {
+            '2': {
+                name: 'Name 2',
+                title: 'Title 2',
+            },
+        },
+    };
+    chai.expect(reducer(state, action)).to.deep.equal(expected);
+})
