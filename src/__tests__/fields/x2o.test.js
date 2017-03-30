a50d656df89811ec82447e3a371c91d72cfc9293
jssuzanne <jssuzanne@anybox.fr>
2017-03-30 16:04:48 +0200

    Update unit test for M2O

--- src/__tests__/fields/x2o.test.js
+++ src/__tests__/fields/x2o.test.js
@@ -15,76 +15,152 @@ import {Provider} from 'react-redux';
 import reducers from '../../reducers';
 
 jest.mock('react-select', () => {
-    return () => {return <div>Select component</div>}
+    return (props) => {return <div {...props}></div>}
 })
-
 test('getField for List', () => {
     const store = createStore(combineReducers(reducers));
     const component = renderer.create(
         <Provider store={store}>
-            {getField('List', 'Many2One', {}, 'Test')}
+            {getField('List', 'Many2One', {model: 'Test', field: 'name'}, '1')}
         </Provider>
     )
     let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    store.dispatch({
+        'type': 'UPDATE_DATA',
+        'model': 'Test',
+        'data': {
+            '1': {
+                'name': "todo 1",
+            },
+        },
+    });
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
 test('getField for Thumbnail', () => {
     const store = createStore(combineReducers(reducers));
     const component = renderer.create(
         <Provider store={store}>
-            {getField('Thumbnail', 'Many2One', {}, 'Test')}
+            {getField('Thumbnail', 'Many2One', {model: 'Test', field: 'name'}, '1')}
         </Provider>
     )
     let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    store.dispatch({
+        'type': 'UPDATE_DATA',
+        'model': 'Test',
+        'data': {
+            '1': {
+                'name': "todo 1",
+            },
+        },
+    });
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
 test('getField for Thumbnail', () => {
     const store = createStore(combineReducers(reducers));
     const component = renderer.create(
         <Provider store={store}>
-            {getField('Thumbnail', 'Many2One', {label: 'Test'}, 'Test')}
+            {getField('Thumbnail', 'Many2One', {label: 'Test', model: 'Test', field: 'name'}, '1')}
         </Provider>
     )
     let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    store.dispatch({
+        'type': 'UPDATE_DATA',
+        'model': 'Test',
+        'data': {
+            '1': {
+                'name': "todo 1",
+            },
+        },
+    });
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
 test('getField for Form', () => {
     const store = createStore(combineReducers(reducers));
     const component = renderer.create(
         <Provider store={store}>
-            {getField('Form', 'Many2One', {}, 'Test')}
+            {getField('Form', 'Many2One', {model: 'Test', field: 'name'}, '1')}
         </Provider>
     )
     let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    store.dispatch({
+        'type': 'UPDATE_DATA',
+        'model': 'Test',
+        'data': {
+            '1': {
+                'name': "todo 1",
+            },
+        },
+    });
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
 test('getField for Form readonly', () => {
     const store = createStore(combineReducers(reducers));
     const component = renderer.create(
         <Provider store={store}>
-            {getField('Form', 'Many2One', {readonly: true, label: 'Test'}, 'Test')}
+            {getField('Form', 'Many2One', {model: 'Test', field: 'name', readonly: true, label: 'Test'}, '1')}
         </Provider>
     )
     let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    store.dispatch({
+        'type': 'UPDATE_DATA',
+        'model': 'Test',
+        'data': {
+            '1': {
+                'name': "todo 1",
+            },
+        },
+    });
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
 test('getField for Form required', () => {
     const store = createStore(combineReducers(reducers));
     const component = renderer.create(
         <Provider store={store}>
-            {getField('Form', 'Many2One', {required: '1', label: 'Test'}, 'Test')}
+            {getField('Form', 'Many2One', {model: 'Test', field: 'name', required: '1', label: 'Test'}, '1')}
         </Provider>
     )
     let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    store.dispatch({
+        'type': 'UPDATE_DATA',
+        'model': 'Test',
+        'data': {
+            '1': {
+                'name': "todo 1",
+            },
+        },
+    });
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
 test('getField for Form required with empty value', () => {
     const store = createStore(combineReducers(reducers));
     const component = renderer.create(
         <Provider store={store}>
-            {getField('Form', 'Many2One', {required: '1', label: 'Test'}, '')}
+            {getField('Form', 'Many2One', {model: 'Test', field: 'name', required: '1', label: 'Test'}, '')}
         </Provider>
     )
     let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    store.dispatch({
+        'type': 'UPDATE_DATA',
+        'model': 'Test',
+        'data': {
+            '1': {
+                'name': "todo 1",
+            },
+        },
+    });
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
