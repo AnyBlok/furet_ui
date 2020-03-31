827f2b8f295e44c732e0c77f35dd9fea3cb121b0
jssuzanne <js.suzanne@gmail.com>
2020-03-31 20:09:00 +0200

    Fixed rendering

--- tests/unit/fields/relationships/one2many.spec.js
+++ tests/unit/fields/relationships/one2many.spec.js
@@ -1,33 +1,33 @@
-import { mount } from "@vue/test-utils";
+import { renderToString } from "@vue/server-test-utils";
 import { getComponentPrototype } from "@/components/factory";
 
 const localVue = global.localVue;
 const store = global.store;
 
 describe("One2Many field for form View", () => {
-
-  const wrapper = mount(getComponentPrototype("furet-ui-form-field-one2many"), {
-    store,
-    localVue,
-    propsData: {
-      resource: {},
-      data: {
-        test: [{id: 1}],
+  const component = getComponentPrototype("furet-ui-form-field-one2many");
+  it("test furet ui form field one2many", async () => {
+    const wrapper = await renderToString(component, {
+      store,
+      localVue,
+      propsData: {
+        resource: {
+          name: 'test'
+        },
+        data: {
+          test: [{id: 1}]
+        },
+        config: {}
       },
-      config: {
-        name: 'test',
+      provide: {
+        partIsReadonly: () => {
+          return false;
+        },
+        updateChangeState: () => {},
+        getEntry: () => {return {}},
+        getNewEntry: () => {return {}}
       }
-    },
-    provide: {
-      partIsReadonly: () => {
-        return false;
-      },
-      updateChangeState: () => {
-      },
-    }
-  });
-
-  it("test furet ui form field string", () => {
-    expect(wrapper.element).toMatchSnapshot();
+    });
+    expect(wrapper).toMatchSnapshot();
   });
 });
