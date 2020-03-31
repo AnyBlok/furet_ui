673731ec55744c35cfdaf7e5b0e86e50ce78b170
Pierre Verkest <pverkest@anybox.fr>
2020-03-31 17:55:34 +0200

    new: [test] #3 test emit in resource.custom

--- tests/unit/resource/custom.spec.js
+++ tests/unit/resource/custom.spec.js
@@ -4,17 +4,25 @@ import { getComponentPrototype } from "@/components/factory";
 const localVue = global.localVue;
 const store = global.store;
 
+store.state.resource[1] = { component: "b-table" };
+const wrapper = mount(getComponentPrototype("furet-ui-resource-custom"), {
+  store,
+  localVue,
+  propsData: {
+    id: 1,
+    manager: {}
+  }
+});
+
 describe("Custom resource", () => {
   it("test furet ui resource custom", () => {
-    store.state.resource[1] = { component: "b-table" };
-    const wrapper = mount(getComponentPrototype("furet-ui-resource-custom"), {
-      store,
-      localVue,
-      propsData: {
-        id: 1,
-        manager: {}
-      }
-    });
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("test update-query-string event properly raised", async () => {
+    wrapper.vm.updateQueryString("test");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted("update-query-string").length).toBe(1);
+    expect(wrapper.emitted("update-query-string")[0]).toEqual(["test"]);
   });
 });
