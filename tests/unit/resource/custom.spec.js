422b416bdb1f9808f8270944de310e9a4adafdbe
Pierre Verkest <pverkest@anybox.fr>
2020-03-31 18:00:01 +0200

    fix: [test] #3 lint extra import not used

--- tests/unit/resource/custom.spec.js
+++ tests/unit/resource/custom.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, mount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import { getComponentPrototype } from "@/components/factory";
 
 const localVue = global.localVue;
---
+++ tests/unit/resource/list.spec.js
@@ -0,0 +1,18 @@
+// import { shallowMount, mount } from "@vue/test-utils";
+// import { getComponentPrototype } from "@/components/factory";
+
+// const localVue = global.localVue;
+// const store = global.store;
+
+// describe("List resource", () => {
+//   it("test furet ui resource list", () => {
+//     const wrapper = shallowMount(
+//       getComponentPrototype("furet-ui-resource-list"),
+//       {
+//         localVue,
+//         store
+//       }
+//     );
+//     expect(wrapper.element).toMatchSnapshot();
+//   });
+// });
