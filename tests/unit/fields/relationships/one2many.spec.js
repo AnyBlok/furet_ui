55384a7ce9df80adaa2d1e1fb6732f03c68ed680
jssuzanne <js.suzanne@gmail.com>
2020-04-01 12:29:42 +0200

    Refactor unit test

--- tests/unit/fields/relationships/one2many.spec.js
+++ tests/unit/fields/relationships/one2many.spec.js
@@ -17,110 +17,48 @@ const getEntry = (model, pk) => {
 
 describe("Field.One2Many for Resource.List", () => {
   const Component = getComponentPrototype("furet-ui-list-field-one2many");
-  it("Empty", () => {
-    const wrapper = mount(Component, {
+  const getOptions = (data, hidden, style) => {
+    return {
       store,
       localVue,
       propsData: {
         resource: {},
         data: {
-          test: [],
+          test: data,
         },
         config: {
+          hidden,
+          style,
           model: 'Model.1',
-          name: 'test'
+          name: 'test',
+          display: "'Go to => ' + fields.title"
         }
       },
       provide: {
         getEntry,
       }
-    });
+    }
+  }
+
+  it("Empty", () => {
+    const wrapper = mount(Component, getOptions([], false, undefined))
     expect(wrapper.element).toMatchSnapshot();
   });
   it("With one value", () => {
-    const wrapper = mount(Component, {
-      store,
-      localVue,
-      propsData: {
-        resource: {},
-        data: {
-          test: [{id: 1}],
-        },
-        config: {
-          model: 'Model.1',
-          name: 'test',
-          display: "'Go to => ' + fields.title"
-        }
-      },
-      provide: {
-        getEntry,
-      }
-    });
+    const wrapper = mount(Component, getOptions([{id: 1}], false, undefined))
     expect(wrapper.element).toMatchSnapshot();
   });
   it("With two value", () => {
-    const wrapper = mount(Component, {
-      store,
-      localVue,
-      propsData: {
-        resource: {},
-        data: {
-          test: [{id: 1}, {id: 2}],
-        },
-        config: {
-          model: 'Model.1',
-          name: 'test',
-          display: "'Go to => ' + fields.title"
-        }
-      },
-      provide: {
-        getEntry,
-      }
-    });
+    const wrapper = mount(Component, getOptions([{id: 1}, {id: 2}], false, undefined))
     expect(wrapper.element).toMatchSnapshot();
   });
   it("Hidden", () => {
-    const wrapper = mount(Component, {
-      store,
-      localVue,
-      propsData: {
-        resource: {},
-        data: {
-          test: [{id: 1}, {id: 2}],
-        },
-        config: {
-          hidden: true,
-          model: 'Model.1',
-          name: 'test',
-          display: "'Go to => ' + fields.title"
-        }
-      },
-      provide: {
-        getEntry,
-      }
-    });
+    const wrapper = mount(Component, getOptions([{id: 1}, {id: 2}], true, undefined))
     expect(wrapper.element).toMatchSnapshot();
   });
   it("With style", () => {
-    const wrapper = mount(Component, {
-      store,
-      localVue,
-      propsData: {
-        resource: {},
-        data: {
-          test: [{id: 1}, {id: 2}],
-        },
-        config: {
-          model: 'Model.1',
-          name: 'test',
-          display: "'Go to => ' + fields.title",
-          style: "`color: ${fields.color};`"
-        }
-      },
-      provide: {
-        getEntry,
-      }
-    });
+    const style = "`color: ${fields.color};`";
+    const wrapper = mount(Component, getOptions([{id: 1}, {id: 2}], false, style))
     expect(wrapper.element).toMatchSnapshot();
   });
 });
