a35ee225d5d75b849e78028ce305bd9e06cf3982
Pierre Verkest <pverkest@anybox.fr>
2020-04-01 16:30:25 +0200

    new: [test] #3 improve Field.String for Resource.Form

--- tests/unit/fields/string.spec.js
+++ tests/unit/fields/string.spec.js
@@ -4,24 +4,76 @@ import { getComponentPrototype } from "@/components/factory";
 const localVue = global.localVue;
 const store = global.store;
 
-const wrapper = mount(getComponentPrototype("furet-ui-form-field-string"), {
-  store,
-  localVue,
-  propsData: {
-    resource: {},
-    data: {},
-    config: {}
-  },
-  provide: {
+describe("Field.String for Resource.Form", () => {
+  const FormStringField = getComponentPrototype("furet-ui-form-field-string");
+  const provide = {
     partIsReadonly: () => {
       return false;
     },
     updateChangeState: () => {}
-  }
-});
+  };
 
-describe("String field", () => {
-  it("test furet ui form field string", () => {
+  it("Empty", () => {
+    const wrapper = mount(FormStringField, {
+      store,
+      localVue,
+      provide,
+      propsData: {
+        resource: {},
+        data: {},
+        config: {}
+      }
+    });
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("With value, label, readonly", () => {
+    const wrapper = mount(FormStringField, {
+      store,
+      localVue,
+      provide,
+      propsData: {
+        resource: {
+          readonly: true
+        },
+        config: {
+          name: "fieldName",
+          label: "My field label"
+        },
+        data: {
+          fieldName: "A value"
+        }
+      }
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("With value, label and icon", () => {
+    const wrapper = mount(FormStringField, {
+      store,
+      localVue,
+      provide,
+      propsData: {
+        resource: {
+          readonly: false
+        },
+        config: {
+          maxlength: 10,
+          placeholder: "An explicit placeholder",
+          name: "fieldName",
+          label: "My field label",
+          icon: "user"
+        },
+        data: {
+          fieldName: "A value"
+        }
+      }
+    });
+    expect(wrapper.element).toMatchSnapshot();
+    expect(wrapper.find("input").element.value).toBe("A value");
+    expect(wrapper.find("input").element.placeholder).toBe(
+      "An explicit placeholder"
+    );
   });
 });
