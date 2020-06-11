000926f1418cb024d8de0b83999973ae021deb5b
Pierre Verkest <pverkest@anybox.fr>
2020-06-11 09:39:01 +0200

    #51: freeze datetime in buefy components

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -9,9 +9,16 @@ import { createStore } from "@/store";
 import { i18n } from "@/i18n";
 import PluginDispatch from "@/plugins/dispatch.js";
 
+// freeze time in buefy unittest test to get predictable snapshot
+const dateCreator = () => new Date("2020-06-10T16:22:11.000+02:00");
+
 // shared localVue
 global.localVue = createLocalVue();
-global.localVue.use(Buefy, { defaultIconPack: "fa" });
+global.localVue.use(Buefy, {
+  defaultIconPack: "fa",
+  defaultDatetimeCreator: dateCreator,
+  defaultDateCreator: dateCreator,
+});
 global.localVue.use(Notifications);
 global.localVue.use(Vuex);
 global.localVue.use(Router);
@@ -19,10 +26,10 @@ global.localVue.use(Router);
 const store = createStore();
 const router = createRouter(store, routes);
 
-i18n.locale = 'en'
+i18n.locale = "en";
 global.localVue.use(PluginDispatch, { router, store, i18n });
 global.store = store;
 global.router = router;
 
-document.execCommand = () => {}
-document.getSelection = () => {}
+document.execCommand = () => {};
+document.getSelection = () => {};
